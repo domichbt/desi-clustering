0608c4e3075f44fb7d42c0796eb1a629d74fed9b
AlbertoRosado1 <ar652820@ohio.edu>
2026-04-07 11:52:22 -0700

    now can plot particle2_correlation

--- clustering_statistics/plotting_tools.py
+++ clustering_statistics/plotting_tools.py
@@ -4,7 +4,7 @@ import lsstypes as types
 from clustering_statistics import tools
 
 
-def get_means_covs(kind, versions, tracer, zrange, region, stats_dir, project='', rebin=1):
+def get_means_covs(kind, versions, tracer, zrange, region, stats_dir, project='', ells=(0,2,4), rebin=1):
     means, covs = {}, {}
     for version in versions:
         kw = {'tracer': tracer}
@@ -19,9 +19,13 @@ def get_means_covs(kind, versions, tracer, zrange, region, stats_dir, project=''
             kw['basis'] = 'sugiyama-diagonal'
             kw['auw'] = False
         fns = tools.get_stats_fn(kind=kind, stats_dir=stats_dir, project=project, zrange=zrange, region=region, **kw, imock='*')
-        # print(fns[0])
         stats = list(map(types.read, fns))
-        means[version] = types.mean(stats).select(k=slice(0, None, rebin))
+        # print(fns[0])
+        if 'particle2_correlation' in kind:
+            stats = [stat.project(ells=ells) for stat in stats]
+            means[version] = types.mean(stats).select(s=slice(0, None, rebin))
+        else:
+            means[version] = types.mean(stats).select(k=slice(0, None, rebin))
         if len(stats) > 1:
             covs[version] = types.cov(stats).at.observable.match(means[version])
         else:
@@ -45,6 +49,7 @@ def plot_stats(kind, versions, tracer, zrange, region, stats_dir, project='', el
         else:
             lax = axes[:, ax_col]
     k_exp = 1 if scaling == 'kpk' else 0
+    s_exp = 2
     if 'mesh2_spectrum' in kind:
         means, covs = get_means_covs(kind, versions, tracer, zrange, region, stats_dir, project=project, rebin=rebin)
         versions = list(means)
@@ -122,6 +127,39 @@ def plot_stats(kind, versions, tracer, zrange, region, stats_dir, project='', el
                 x = pole.coords('k')[..., 0]
                 ax.plot(x, (pole.value() - means[reference].get(ell).value()).real / std, color=colors[version], linestyle=linestyles[version], lw=lw)
         lax[-1].set_xlabel(r'$k$ [$h/\mathrm{Mpc}$]')
+
+    elif 'particle2_correlation' in kind:
+        means, covs = get_means_covs(kind, versions, tracer, zrange, region, stats_dir, project=project, ells=ells, rebin=rebin)
+        versions = list(means)
+        if title is None:
+            lax[0].set_title(f'{tracer} in {region} {zrange[0]:.1f} < z < {zrange[1]:.1f}')
+        else:
+            lax[0].set_title(title)
+        for ill, ell in enumerate(ells):
+            ax = lax[2 * ill]
+            ax.set_ylabel(rf'$s^2 \xi_{ell:d}(s)$ [$(\mathrm{{Mpc}}/h)^2$]')
+            for iversion, version in enumerate(versions):
+                if ell not in means[version].ells: continue
+                pole = means[version].get(ell)
+                value = pole.coords('s')**s_exp * pole.value().real
+                if 'data' in version or version == reference:
+                    std = pole.coords('s')**s_exp * covs[reference].at.observable.get(ell).std().real
+                    ax.fill_between(pole.coords('s'), value - std, value + std, color=colors[version], alpha=0.2)
+                ax.plot(pole.coords('s'), value, color=colors[version], linestyle=linestyles[version], label=version, lw=lw)
+            if ill == 0: ax.legend(frameon=False, ncol=1)
+            ax.grid(True)
+            ax = lax[2 * ill + 1]
+            ax.set_ylabel(rf'$\Delta P_{ell:d} / \sigma(k)$')
+            ax.grid(True)
+            ax.set_ylim(*ylim)
+            for iversion, version in enumerate(versions):
+                if 'data' in version or version == reference: continue
+                pole = means[version].get(ell)
+                # std = covs[reference].at.observable.get(ell).std().real
+                std = covs[reference].at.observable.get(ell).std().real
+                # print(tracer,zrange,version,kind,ell,std.min())
+                ax.plot(pole.coords('s'), (pole.value() - means[reference].get(ell).value()).real / std, color=colors[version], linestyle=linestyles[version], lw=lw)
+        lax[-1].set_xlabel(r'$s$ [$h^{-1}\mathrm{Mpc}$]')
 
     if save_fn and figure is None:
         plt.tight_layout()
