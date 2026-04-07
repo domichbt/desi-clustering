33a2cb65f37238bb97e11e296759d8972c833354
AlbertoRosado1 <ar652820@ohio.edu>
2026-04-07 12:42:07 -0700

    typo

--- clustering_statistics/helper_scripts/rename_dubious_folders.sh
+++ clustering_statistics/helper_scripts/rename_dubious_folders.sh
@@ -4,7 +4,7 @@
 
 base_dir=/global/cfs/cdirs/desi/science/cai/desi-clustering/dr2/summary_statistics/full_shape/base/holi-v3-altmtl/
 mapfile -t mocks_list < dubious_holi-v3-altmtl.txt
-for imock in "${mock_list[@]}"; do
+for imock in "${mocks_list[@]}"; do
     echo "Renaming $imock"
     mv $base_dir/mock$imock/ $base_dir/dubious_mock_$imock
 done
